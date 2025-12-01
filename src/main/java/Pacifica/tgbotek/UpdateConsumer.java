502db332caec910d6f8e36453a91d2efe8eb899b
Pacfifca <shelkaev86@gmail.com>
2025-12-02 02:15:42 +0300

    0212

--- src/main/java/Pacifica/tgbotek/UpdateConsumer.java
+++ src/main/java/Pacifica/tgbotek/UpdateConsumer.java
@@ -3,11 +3,17 @@ package Pacifica.tgbotek;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
-import org.telegram.telegrambots.meta.api.methods.send.SendMediaGroup;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+import java.util.List;
 
 @Component
 public class UpdateConsumer implements LongPollingSingleThreadUpdateConsumer {
@@ -23,16 +29,103 @@ private final TelegramClient telegramClient;
     @Override
     public void consume(Update update) {
 
-        System.out.printf(
-                "Пришло сообщшение %s от %s%n",
-                update.getMessage().getText(),
-                update.getMessage().getChatId()
-        );
-        var chatId = update.getMessage().getChatId();
+        if (update.hasMessage()){
+            String messageText = update.getMessage().getText();
+            Long chatId = update.getMessage().getChatId();
+
+            if (messageText.equals("/start")){
+                sendMainMenu(chatId);
+            } else {
+                sendMessage(chatId,"Не понял");
+            }
+        } else if (update.hasCallbackQuery()){
+            handleCallbackQuery(update.getCallbackQuery());
+        }
+    }
+
+    private void handleCallbackQuery(CallbackQuery callbackQuery) {
+        var data = callbackQuery.getData();
+        var chatId = callbackQuery.getFrom().getId();
+        var user = callbackQuery.getFrom();
+        switch(data){
+            case "my_name" -> sendMyName(chatId, user);
+            case "showEmployeeList" -> sendEmployeesList(chatId);
+            case "showTasksList" -> sendTaskList(chatId);
+            case "getFilesList" -> sendFilesList(chatId);
+            default -> sendMessage(chatId, "Неизвестная комманда");
+        }
+
+
+
+    }
+
+    private void sendMessage(Long chatId, String messageText){
         SendMessage message = SendMessage.builder()
-                .text("привет, твоя мама " + update.getMessage().getText())
+                .text(messageText)
                 .chatId(chatId)
                 .build();
+
+        try {
+            telegramClient.execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private void sendFilesList(Long chatId) {
+        sendMessage(chatId, "присылаю список файлов...");
+
+    }
+
+    private void sendTaskList(Long chatId) {
+        sendMessage(chatId, "присылаю список задач...");
+
+    }
+
+    private void sendEmployeesList(Long chatId) {
+        sendMessage(chatId,"прислыаю список сотрудиков...");
+
+    }
+
+    private void sendMyName(Long chatId, User user) {
+
+    }
+
+    private void sendMainMenu(Long chatId) {
+        SendMessage message = SendMessage.builder()
+                .text("Выберите действие:")
+                .chatId(chatId)
+                .build();
+
+        var button1 = InlineKeyboardButton.builder()
+                .text("отобразить сотрудников")
+                .callbackData("showEmployeeList")
+                .build();
+        var button2 = InlineKeyboardButton.builder()
+                .text("актуальные задачи")
+                .callbackData("showTasksList")
+                .build();
+        var button3 = InlineKeyboardButton.builder()
+                .text("файлы")
+                .callbackData("getFilesList")
+                .build();
+        var button4 = InlineKeyboardButton.builder()
+                .text("мой профиль")
+                .callbackData("my_name")
+                .build();
+
+        List<InlineKeyboardRow> keyboardRows = List.of(
+                new InlineKeyboardRow(button1),
+                new InlineKeyboardRow(button2),
+                new InlineKeyboardRow(button3),
+                new InlineKeyboardRow(button4)
+        );
+
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup(keyboardRows);
+
+        message.setReplyMarkup(markup);
+
         try {
             telegramClient.execute(message);
         } catch (TelegramApiException e) {
